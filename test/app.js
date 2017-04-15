737e66b93d602e54f7f98d5f7539b5b7be35efcf
Michael Stramel <stramel@users.noreply.github.com>
2017-04-15 15:33:58 -0500

    Update app.js

--- test/app.js
+++ test/app.js
@@ -23,7 +23,7 @@ describe('generator-polymer-init-custom-build:app', function() {
 
   it('creates files', function() {
     assert.file([
-      '.travis.yml',
+      'browserslist',
       'gulpfile.js',
       'package.json',
       'README.md',
