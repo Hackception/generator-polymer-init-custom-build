b6e45bd4a6ce14fa7da873d854ffd7a3fab2ce1b
Michael Stramel <stramel@users.noreply.github.com>
2017-04-15 15:16:29 -0500

    TEST

--- test/app.js
+++ test/app.js
@@ -23,7 +23,7 @@ describe('generator-polymer-init-custom-build:app', function() {
 
   it('creates files', function() {
     assert.file([
-      'browserslist',
+      '.travis.yml',
       'gulpfile.js',
       'package.json',
       'README.md',
